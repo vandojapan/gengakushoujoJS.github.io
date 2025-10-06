a4386417ec8f0195699f64013fa88b87cf66141d
vandojapan <30983282+vandojapan@users.noreply.github.com>
2025-10-07 01:52:15 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 export const CONFIG={canvasWidth:640,canvasHeight:480,tileSize:16,gravity:0.35,moveSpeed:2.0,jumpSpeed:-6.0,
-dataPaths: { Map0005: "assets/data/Map0001.xml" },
+dataPaths: { Map0005: "assets/data/Map0005.xml" },
 };export const KEY={LEFT:["ArrowLeft","a","A"],RIGHT:["ArrowRight","d","D"],JUMP:["z","Z"," "],};
